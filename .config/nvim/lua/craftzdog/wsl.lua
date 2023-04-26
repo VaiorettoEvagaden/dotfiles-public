feb41de709f1fc6774aa4e0bdf1b553a9a254cab
冯学远 <xueyuanf@qq.com>
2023-04-26 19:47:24 +0800

    change wsl clipboard

--- .config/nvim/lua/craftzdog/wsl.lua
+++ .config/nvim/lua/craftzdog/wsl.lua
@@ -1,6 +1,24 @@
-vim.cmd [[
-  augroup Yank
-  autocmd!
-  autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
-  augroup END
-]]
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = 'WSLClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'pwsh.exe -NoProfile -NonInteractive -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'pwsh.exe -NoProfile -NonInteractive -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = false
+}
+
+-- vim.g.clipboard = {
+--   copy = {
+--     ["+"] = "win32yank.exe -i --crlf",
+--     ["*"] = "win32yank.exe -i --crlf",
+--   },
+--   paste = {
+--     ["+"] = "win32yank.exe -o --lf",
+--     ["*"] = "win32yank.exe -o --lf",
+--   },
+-- }
